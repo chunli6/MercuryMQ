ba4946a46ccd444d53c310406cb9d5dae06569d5
Denny Kwon <info@tsunami234.com>
2014-08-23 12:58:15 -0400

    abstracted individual angular modules into on angular app with controllers

---
+++ site/js/app.js
@@ -0,0 +1,51 @@
+var app = angular.module('TextAlert', ['HomeViewController', 'AboutViewController']);
+
+app.directive('menu', function() {
+	  return {
+	      restrict: 'AE',
+	      replace: true,
+	      templateUrl: '/site/partials/menu.html'
+	  };
+});
+
+
+app.directive('spinner', function() {
+    return {
+        restrict: 'A',
+        replace: true,
+        scope: {
+            startSpinner: '=spin'
+        },
+        template: '<div></div>',
+        link: function (scope, element, attrs) {
+            var opts = {
+              lines: 13,
+              length: 20,
+              width: 10,
+              radius: 30,
+              corners: 1,
+              rotate: 0,
+              direction: 1,
+              color: '#000',
+              speed: 1,
+              trail: 60,
+              shadow: false,
+              hwaccel: false,
+              className: 'spinner',
+              zIndex: 2e9
+            };
+            var spinner = new Spinner(opts);
+            scope.$watch('startSpinner', function (startSpinner) {
+                if (startSpinner) {
+                    spinner.spin(element[0]);
+                } 
+                else {
+                    spinner.stop();
+                }
+            });
+        }
+    }
+});
+
+
+
--- site/js/controllers/about.js
+++ site/js/controllers/about.js
@@ -1,20 +1,13 @@
-var homeController = angular.module('HomeViewController', []);
+var aboutController = angular.module('AboutViewController', []);
 
 
-homeController.controller('HomeController', ['$scope', '$http', function($scope, $http){
+aboutController.controller('AboutViewController', ['$scope', '$http', function($scope, $http){
 	$scope.loading = false;
     $scope.profile = {'email':'', 'password':'', 'name':'', 'loggedIn':'no', 'number':''};
-    $scope.newOrder = {'from':'', 'order':'', 'address':''};
-    $scope.selectedVenue = null;
-    $scope.featuredVenues = [{'name':'Chipotle', 'city':'Ramsey', 'state':'NJ', 'description':'Chipotle\'s menu consists of five items: burritos, fajita burritos, burritos bowls, tacos and salads. Chipotle elevates basic raw ingredients into food that\'s richer through their recipes and cooking techniques.', 'image':'chipotle.png'},
-                             {'name':'Panera Bread', 'city':'Woodcliff Lake', 'state':'NJ', 'description':'Panera offers a variety on their menu of sandwiches, soups and salads. Treat your taste buds to freshly baked flatbread, layered with fresh, bold ingredients. Have Panera delivered to your door and enjoy!', 'image':'panera.png'},
-                             {'name':'CVS', 'city':'Montvale', 'state':'NJ', 'description':'CVS offers a wide range of products from paper towels to sunscreen. Feeling sick and need some Dayquil? No need to get up, just order from Mercury MQ. Need drinks and chips for a party? We\'ve got you covered.', 'image':'cvs.png'},
-                             {'name':'Whole Foods', 'city':'Ridgewood', 'state':'NJ', 'description':'Whole Foods seeks out the finest natural and organic foods avaiable. They maintain the strictest quality standards in the industry. Order healthy organic food for breakfast, lunch and dinner! ', 'image':'wholefoods.png'}];
-    
     
     
     $scope.init = function() {
-    	console.log('HomeController: INIT');
+    	console.log('AboutController: INIT');
     	checkLoggedIn();
     }
     
@@ -115,53 +108,6 @@ homeController.controller('HomeController', ['$scope', '$http', function($scope,
     	
     }
     
-    $scope.order = function() {
-    	console.log('ORDER');
-    	
-    	if ($scope.newOrder.from.length < 10){
-    		alert('Error: Please enter a valid phone number, like this: 201-555-1234');
-    		return false;
-    	}
-
-    	if ($scope.newOrder.order.length == 0){
-    		alert('Error: Please enter a valid order.');
-    		return false;
-    	}
-    	
-    	if ($scope.newOrder.address.length == 0){
-    		alert('Error: Please enter a valid address.');
-    		return false;
-    	}
-    	
-    	$scope.newOrder['venue'] = $scope.selectedVenue.name;
-    	var json = JSON.stringify($scope.newOrder);
-    	console.log(json);
-    	
-    	$scope.loading = true;
-    	
-        var url = '/api/deliveryorders';
-        $http.post(url, json).success(function(data, status, headers, config) {
-            var results = data['results'];
-            var confirmation = results['confirmation'];
-            if (confirmation=='success'){
-            	$scope.loading = false;
-            	alert('Your Order has been received. Thank you!');
-                $scope.newOrder = {'phone':'', 'order':'', 'promo':'', 'address':''};
-            }
-            else {
-                alert(results['message']);
-            }
-        }).error(function(data, status, headers, config) {
-            console.log("error", data, status, headers, config);
-        });
-
-    }
-    
-    $scope.selectVenue = function(index){
-    	$scope.selectedVenue = $scope.featuredVenues[index];
-    	console.log('selectVenue: '+index);
-    	
-    }
 
     $scope.capitalize = function(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
